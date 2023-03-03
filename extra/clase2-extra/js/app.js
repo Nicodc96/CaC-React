15aafb2cd122c00ebe924133776b4f979fdde5a2
Nico Desktop <diaznicox96@gmail.com>
2023-03-03 14:52:04 -0300

    images changed, add more features

--- extra/clase2-extra/js/app.js
+++ extra/clase2-extra/js/app.js
@@ -1,7 +1,10 @@
 const $poster = document.querySelector("#poster");
+const $logo = document.querySelector("#logoAvengers");
 const poster_h = $poster.clientHeight;
 const poster_w = $poster.clientWidth;
 const $avengers_sound = new Audio("./assets/avengers-endgame.mp3");
+const ruta_logo = "./assets/logo-avengers.jpg";
+const ruta_logo_gif = "./assets/logo-avengers-mov.gif";
 
 $avengers_sound.volume = 0.5;
 
@@ -19,4 +22,12 @@ $poster.addEventListener("mouseout", () => {
     $poster.style.transform = "perspective(500px) scale(1) rotateX(0) rotateY(0)";
     $avengers_sound.pause();
     $avengers_sound.currentTime = 0;
+});
+
+$logo.addEventListener("mouseover", (e) => {
+    $logo.src = ruta_logo_gif;
+});
+
+$logo.addEventListener("mouseout", (e) => {
+    $logo.src = ruta_logo;
 });
