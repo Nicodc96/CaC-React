ee92cbe6be51135b65d4ae0bcb3085e247961e92
Nico Desktop <diaznicox96@gmail.com>
2023-03-18 17:38:27 -0300

    add doxygen on exercises in class 4

--- clase4/ejercicios.js
+++ clase4/ejercicios.js
@@ -2,7 +2,6 @@
     EJERCICIO EXTRA : RECOMENDAMOS PELICULA - SERIE O LIBRO
     UTILIZAMOS SWITCH
 */
-// reemplaza `null` por la respuesta
 const recomendarPelicula = (pelicula) =>{
     const res_pelicula = (pelicula) => {
         switch(pelicula){
@@ -39,184 +38,227 @@ const nuevaMultiplicacion = 10 * 4 === 40;
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === 1;
 
-// function devolverString(str) {
-//   // "Return" la string provista: str
-//   // Tu código:
-//     return str;
-// }
+/**
+ * Tomna un valor y devuelve su equivalente en string.
+ * @param {*} str 
+ * @returns {string}
+ */
 const devolverString = str => str.toString();
 
-// function suma(x, y) {
-//   // "x" e "y" son números
-//   // Suma "x" e "y" juntos y devuelve el valor
-//   // Tu código:
-// }
+/**
+ * Suma 2 números.
+ * @param {Number} x primer número
+ * @param {Number} y segundo número
+ * @returns {Number | 'NaN'} El resultado de la suma, en caso de que alguno de los valores no
+ * sea un número, devuelve *NaN*
+ */
 const suma = (x, y) => Number(x) + Number(y);
 
 // function resta(x, y) {
 //   // Resta "x" de "y" y devuelve el valor
 //   // Tu código:
 // }
+/**
+ * Resta 2 números.
+ * @param {Number} x primer número
+ * @param {Number} y segundo número
+ * @returns {Number | 'NaN'} El resultado de la resta, en caso de que alguno de los valores no
+ * sea un número, devuelve *NaN*
+ */
 const resta = (x, y) => Number(x) - Number(y);
 
-// function multiplica(x, y) {
-//   // Multiplica "x" por "y" y devuelve el valor
-//   // Tu código:
-// }
+/**
+ * Realiza una multiplicación de 2 números.
+ * @param {Number} x primer número
+ * @param {Number} y segundo número
+ * @returns {Number | 'NaN'} El resultado de la multiplicación, en caso de que alguno de los valores no
+ * sea un número, devuelve *NaN*
+ */
 const multiplica = (x, y) => Number(x) * Number(y);
 
 // function divide(x, y) {
 //   // Divide "x" entre "y" y devuelve el valor
 //   // Tu código:
 // }
+/**
+ * Realiza una divisón de 2 números.
+ * @param {Number} x primer número
+ * @param {Number} y segundo número
+ * @returns {Number | 'NaN'} El resultado de la divisón, en caso de que alguno de los valores no
+ * sea un número o el valor de **y** sea 0, devuelve *NaN*
+ */
 const divide = (x, y) => Number(y) !== 0 ? Number(x) / Number(y) : NaN;
 
-// function sonIguales(x, y) {
-//   // Devuelve "true" si "x" e "y" son iguales
-//   // De lo contrario, devuelve "false"
-//   // Tu código:utilizar if y else
-// }
+/**
+ * Verifica si dos valores son iguales.
+ * @param {*} x primer valor
+ * @param {*} y segundo valor
+ * @returns {Boolean} **true** si se verifica, de lo contrario **false**.
+ */
 const sonIguales = (x, y) => x == y;
 
-// function tienenMismaLongitud(str1, str2) {
-//   // Devuelve "true" si las dos strings tienen la misma longitud
-//   // De lo contrario, devuelve "false"
-//   // Tu código:
-
-// }
+/**
+ * Verifica si dos strings poseen la misma longitud.
+ * @param {string} str1 
+ * @param {string} str2 
+ * @returns {Boolean} **true** si se verifica, de lo contrario **false**.
+ */
 const tienenMismaLongitud = (str1, str2) => str1.length == str2.length;
 
-// function menosQueNoventa(num) {
-//   // Devuelve "true" si el argumento de la función "num" es menor que noventa
-//   // De lo contrario, devuelve "false"
-//   // Tu código:
-// }
-const menosQueNoventa = (num) => num < 90;
+/**
+ * Verifica si el número por parámetro es exclusivamente menor a 90.
+ * @param {number} num 
+ * @returns {Boolean} **true** si se verifica, de lo contrario o si no es número **false**.
+ */
+const menosQueNoventa = (num) => !isNaN(num) && num < 90;
 
-// function mayorQueCincuenta(num) {
-//   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
-//   // De lo contrario, devuelve "false"
-//   // Tu código:
-// }
-const mayorQuecincuenta = (num) => num > 50;
+/**
+ * Verifica si el número por parámetro es exclusivamente mayor a 50.
+ * @param {number} num 
+ * @returns {Boolean} **true** si se verifica, de lo contrario o si no es número **false**.
+ */
+const mayorQuecincuenta = (num) => !isNaN(num) && num > 50;
 
-// function obtenerResto(x, y) {
-//   // Obten el resto de la división de "x" entre "y"
-//   // Tu código:
-// }
-const obtenerResto = (x, y) => Number(`0.${divide(x, y).toString().split(".")[1]}`);
+/**
+ * Obtiene el valor decimal resultado de una división entre 2 números (máximo 3 dígitos).
+ * @param {Number} x primer número
+ * @param {Number} y segundo número
+ * @returns {Number | 'NaN'} El valor decimal sin el entero. Si alguno de los parámetros no es un número
+ * o no tiene valor decimal devuelve **NaN**
+ */
+const obtenerResto = (x, y) => Number(`0.${divide(x, y).toString().split(".")[1]}`).toFixed(3);
 
-// function esPar(num) {
-//   // Devuelve "true" si "num" es par
-//   // De lo contrario, devuelve "false"
-//   // Tu código:  
-// }
-const esPar = num => Number(num) % 2 == 0;
+/**
+ * Verifica si un número es par. No se considera 0 como valor par.
+ * @param {Number} num 
+ * @returns {Boolean} **true** si se verifica, de lo contrario **false**
+ */
+const esPar = num => Number(num) !== 0 && Number(num) % 2 == 0;
 
-// function esImpar(num) {
-//   // Devuelve "true" si "num" es impar
-//   // De lo contrario, devuelve "false"
-//   // Tu código:
-// }
+/**
+ * Verifica si un número es impar. No se considera 0 como valor impar.
+ * @param {Number} num 
+ * @returns {Boolean} **true** si se verifica, de lo contrario **false**
+ */
 const esImpar = num => !esPar(num);
 
-// function elevarAlCuadrado(num) {
-//   // Devuelve el valor de "num" elevado al cuadrado
-//   // ojo: No es raiz cuadrada!
-//   // Tu código:
-// }
+/**
+ * Eleva un número al cuadrado.
+ * @param {Number} num 
+ * @returns {Number | 'NaN'} El resultado de la potencia. Si el valor no es número
+ * devuelve **NaN**
+ */
 const elevarAlCuadrado = num => Math.pow(Number(num), 2);
 
-// function elevarAlCubo(num) {
-//   // Devuelve el valor de "num" elevado al cubo
-//   // Tu código:
-// }
+/**
+ * Eleva un número al cubo.
+ * @param {Number} num 
+ * @returns {Number | 'NaN'} El resultado de la potencia. Si el valor no es número
+ * devuelve **NaN**
+ */
 const elevarAlCubo = num => Math.pow(Number(num), 3);
 
-// function elevar(num, exponent) {
-//   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
-//   // Tu código:
-// }
+/**
+ * Eleva un número.
+ * @param {Number} num base
+ * @param {Number} exponent exponente  
+ * @returns {Number | 'NaN'} El resultado de la potencia. Si uno o más valores no son números
+ * devuelve **NaN**
+ */
 const elevar = (num, exponent) => Math.pow(Number(num), Number(exponent));
 
-// function redondearNumero(num) {
-//   // Redondea "num" al entero más próximo y devuélvelo
-//   // Tu código:
-// }
+/**
+ * Redondea un número al entero más cercano.
+ * @param {Number} num 
+ * @returns {Number | 'NaN'} El valor del entero más cercano, si el número por parámetro
+ * no es un número devuelve **NaN**
+ */
 const redondearNumero = num => Math.round(num);
 
-// function redondearHaciaArriba(num) {
-//   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
-//   // Tu código:
-// }
+/**
+ * Redondea un número al entero más cercano hacia arriba.
+ * @param {Number} num 
+ * @returns {Number | 'NaN'} El valor del entero más cercano, si el número por parámetro
+ * no es un número devuelve **NaN**
+ */
 const redondearHaciaArriba = num => Math.ceil(num);
 
-// function numeroRandom() {
-//   //Generar un número al azar entre 0 y 1 y devolverlo
-//   //Pista: investigá qué hace el método Math.random()
-// }
+/**
+ * Devuelve un pseudo número aleatorio entre 0 y 1. 
+ */
 const numeroRandom = () => Math.random();
+/**
+ * Verifica si el valor es un número positivo, negativo o 0.
+ * @param {Number} num 
+ * @returns {String | Boolean | 'NaN'} El mensaje correspondiente, **false** si es valor es 0 ó
+ * **NaN** si el valor no es un número.
+ */
+const esPositivo = num => isNaN(num) ? NaN : (num == 0 ? false : (num > 0 ? "Es positivo." : "Es negativo."));
 
-// function esPositivo(numero) {
-//   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
-//   //Si el número es positivo, devolver ---> "Es positivo"
-//   //Si el número es negativo, devolver ---> "Es negativo"
-//   //Si el número es 0, devuelve false
-// }
-const esPositivo = num => num > 0 ? "Es positivo" : "Es negativo";
-
-// function agregarSimboloExclamacion(str) {
-//   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
-//   // Ejemplo: "hello world" pasaría a ser "hello world!"
-//   // Tu código:
-// }
+/**
+ * Agrega una exclamación al final del parámetro enviado.
+ * @param {*} str 
+ * @returns {string} El valor string del parámetro con '!' al final.
+ */
 const agregarSimboloExclamacion = str => `${str}!`;
 
-// function combinarNombres(nombre, apellido) {
-//   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
-//   // Ejemplo: "Soy", "Bruce Wayne" -> "Bruce Wayne"
-//   // Tu código:
-// }
-const combinarNombres = (nombre, apellido) => `${nombre[0].toUpperCase()}${nombre.split(nombre[0])[1].toLowerCase()} 
-${apellido[0].toUpperCase()}${apellido.split(apellido[0])[1].toLowerCase()}`;
+/**
+ * Combina dos parámetros string y los devuelve formateado de la siguiente manera: "Nombre Apellido"
+ * @param {*} nombre 
+ * @param {string} apellido 
+ * @returns {undefined | string} String formateado con la primera letra en mayúscula y un espacio entre mensajes. En
+ * caso de recibir otro valor devuelve **undefined**
+ */
+const combinarNombres = (nombre, apellido) => !isNaN(nombre) || !isNaN(apellido) ? undefined : (`${nombre[0].toUpperCase()}${nombre.split(nombre[0])[1].toLowerCase()} 
+${apellido[0].toUpperCase()}${apellido.split(apellido[0])[1].toLowerCase()}`);
 
-// function obtenerSaludo(nombre) {
-//   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
-//   // "Martin" -> "Hola Martin!"
-//   // Tu código:
-// }
-const obtenerSaludo = nombre => `Hola ${nombre[0].toUpperCase()}${nombre.split(nombre[0])[1].toLowerCase()}!`;
+/**
+ * Obtiene un string y devuelve un mensaje saludando.
+ * @param {string} nombre 
+ * @returns {undefined | string} El saludo formateado, si el parámetro no es un string devuelve
+ * **undefined**
+ */
+const obtenerSaludo = nombre => !isNaN(nombre) || !isNaN(apellido) ? undefined :
+(`Hola ${nombre[0].toUpperCase()}${nombre.split(nombre[0])[1].toLowerCase()}!`);
 
-// function obtenerAreaRectangulo(alto, ancho) {
-//   // Retornar el area de un cuadrado teniendo su altura y ancho
-//   // Tu código:
-// }
+/**
+ * Obtiene el area de un rectángulo a partir de su alto y su ancho.
+ * @param {Number} alto 
+ * @param {Number} ancho 
+ * @returns {Number | NaN} El resultado correspondiente, si alguno de los parámetros no es un número
+ * devuelve **NaN**
+ */
 const obtenerAreaRectangulo = (alto, ancho) => multiplica(alto, ancho);
 
-// function retornarPerimetro(lado){
-//   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
-//   //Escribe tu código aquí
-// }
+/**
+ * Calcula el perímetro de un cuadrado según su lado.
+ * @param {Number} lado 
+ * @returns {Number | 'NaN'} El valor del perímetro, si el valor del lado no es un número
+ * devuelve **NaN**
+ */
 const retornarPerimetro = lado => multiplica(lado, 4);
 
-// function areaDelTriangulo(base, altura){
-//   //Desarrolle una función que calcule el área de un triángulo.
-//   //Escribe tu código aquí
-// }
+/**
+ * Calcula el area de un triángulo a partir de su base y su altura.
+ * @param {Number} base 
+ * @param {Number} altura 
+ * @returns {Number | 'NaN'} El valor del area. si alguno de los parámetros no es un número
+ * devuelve **NaN**
+ */
 const areaDelTriangulo = (base, altura) => divide(multiplica(base, altura), 2);
 
-// function deEuroAdolar(euro){
-//   //Supongamos que 1 euro equivale a 1.20 dólares. 
-//   //Escribe un programa que pida al usuario un número de euros y calcule el cambio en dólares.
-//   //Escribe tu código aquí
-// }
-const deEuroADolar = dolar => `Tu cambio en dolares es: $${multiplica(dolar, 1.2).toFixed(2)}`;
+/**
+ * Convierte el valor de Euros (€) a su equivalente en dólares (uSd$) (1€ = 1.2uSd$)
+ * @param {number} euro 
+ * @returns {undefined | string} Un mensaje indicando el valor del cambio, si el valor no es un número
+ * devuelve **undefined**
+ */
+const deEuroADolar = euro => isNaN(euro) ? undefined : `Tu cambio en dolares es: $${multiplica(euro, 1.2).toFixed(2)}`;
 
-// function esVocal(letra){
-//   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
-//   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
-//   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
-//     //si ingresa una consonante muestre en pantalla dato incorrecto
-//   //Escribe tu código aquí
-// }
+/**
+ * Verifica si una letra es una vocal (mayúsculas, acentos y caracteres especiales incluídos).
+ * @param {string} letra 
+ * @returns {string} El mensaje en string indicando si es vocal, en caso de no poder verificarse
+ * devuelve el mensaje informándolo.
+ */
 const esVocal = letra => letra.length == 1 && /^[aeiouáéíóúü]{1}.*/i.test(letra) ? "Es Vocal." : "Dato incorrecto";
