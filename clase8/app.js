51ba9c9ccca8f2f4807560f0d24cc33d2e65e10e
Nico Desktop <diaznicox96@gmail.com>
2023-03-27 16:44:46 -0300

    fetch practice completed in class 8 folder

--- clase8/app.js
+++ clase8/app.js
@@ -1,42 +1,71 @@
-import createTable from "./js/listaDinamica.js";
-import PostsTest from "./class/posts.js";
-import { getPostById } from "./js/fetch.js";
-import { createElementCustom } from "../clase3/js/elementos.js";
+import { getPostById, getPostsByUsersId, getUserById, getAllUsers } from "./js/fetch.js";
+import { crearTabla, limpiarTabla, mostrarDatosTabla } from "./js/functions.js";
 
+/* Botones de POST */
 const contenedorPosteos = document.querySelector("#listaPosteos");
 const btnTraerPostID = document.querySelector("#btnTraerPostId");
 const btnTraerPostUserID = document.querySelector("#btnTraerPostUserId");
+/* Botones de USERS */
+const contenedorUsuarios = document.querySelector("#listaUsuarios");
+const btnTraerUserID = document.querySelector("#btnTraerUserId");
+const btnTraerUserNames = document.querySelector("#btnTraerUserNames");
+const btnTraerUserEmails = document.querySelector("#btnTraerUserEmails");
+const btnTraerUserAdress = document.querySelector("#btnTraerUserAdress");
+const btnTraerUserCompanies = document.querySelector("#btnTraerUserCompanies");
+const listaUsuarios = await getAllUsers();
 
 btnTraerPostID.addEventListener("click", async () => {
     const inputPostId = document.querySelector("#inputTraerPostId");
-    if (isNaN(Number(inputPostId.value))){
-        console.error("Se ha ingresado un valor no numérico.");
+    let inputValue = Number(inputPostId.value);
+    if (inputPostId.value == ""){
+        alert("Se debe ingresar un valor correcto primero.");
     } else{
         limpiarTabla(contenedorPosteos);
-        const jsonData = await getPostById(Number(inputPostId.value));
+        const jsonData = await getPostById(inputValue);
         contenedorPosteos.appendChild(crearTabla(jsonData));
+        inputPostId.value = "";
     }
 });
 
-const crearTabla = (data) => {
-    const table = createElementCustom("table", ["table", "table-striped-columns"], "", {});
-    const tableHead = createElementCustom("thead", ["text-uppercase"], "", {});
-    const tHeadRow = createElementCustom("tr", [], "", {});
-    tableHead.appendChild(tHeadRow);
-    const tableBody = createElementCustom("tbody", [], "", {});
-    const tBodyRow = createElementCustom("tr", [], "", {});
-    tableBody.appendChild(tBodyRow);
-    for (let i = 0; i < Object.keys(data).length; i++){
-        tBodyRow.appendChild(createElementCustom("td", [], data[Object.keys(data)[i]].toString(), {}));
-        tHeadRow.appendChild(createElementCustom("th", [], Object.keys(data)[i], {}));
+btnTraerPostUserID.addEventListener("click", async () => {
+    const inputPostUserId = document.querySelector("#inputTraerPostUserId");
+    let inputValue = Number(inputPostUserId.value);
+    if (inputPostUserId.value == ""){
+        alert("Se debe ingresar un valor correcto primero.");
+    } else{
+        limpiarTabla(contenedorPosteos);
+        const jsonData = await getPostsByUsersId(inputValue);
+        contenedorPosteos.appendChild(crearTabla(jsonData));
+        inputPostUserId.value = "";
     }
-    table.appendChild(tableHead);
-    table.appendChild(tableBody);
-    return table;
-}
+});
 
-const limpiarTabla = contenedor => {
-    while(contenedor.hasChildNodes()){
-        contenedor.removeChild(contenedor.firstElementChild);
+btnTraerUserID.addEventListener("click", async () => {
+    const inputUserId = document.querySelector("#inputTraerUserId");
+    let userId = Number(inputUserId.value);
+    if (inputUserId.value == ""){
+        alert("Se debe ingresar un valor correcto primero.");
+    } else{
+        limpiarTabla(contenedorUsuarios);
+        const jsonData = await getUserById(userId);
+        contenedorUsuarios.appendChild(crearTabla(jsonData));
+        inputUserId.value = "";
     }
-}
+});
+
+btnTraerUserNames.addEventListener("click", () => {    
+    limpiarTabla(contenedorUsuarios);
+    contenedorUsuarios.appendChild(mostrarDatosTabla(listaUsuarios.map(user => user.name), ["nombres"]));
+});
+btnTraerUserEmails.addEventListener("click", () => {    
+    limpiarTabla(contenedorUsuarios);
+    contenedorUsuarios.appendChild(mostrarDatosTabla(listaUsuarios.map(user => user.email), ["emails"]));
+});
+btnTraerUserAdress.addEventListener("click", () => {   
+    limpiarTabla(contenedorUsuarios);
+    contenedorUsuarios.appendChild(mostrarDatosTabla(listaUsuarios.map(user => user.address), []));
+});
+btnTraerUserCompanies.addEventListener("click", () => {    
+    limpiarTabla(contenedorUsuarios);
+    contenedorUsuarios.appendChild(mostrarDatosTabla(listaUsuarios.map(user => user.company), []));
+});
--- clase8/js/fetch.js
+++ clase8/js/fetch.js
@@ -1,5 +1,5 @@
 const URL_POSTS = "https://jsonplaceholder.typicode.com/posts";
-const URL_USERS = "https://jsonplaceholder.typicode.com/users/";
+const URL_USERS = "https://jsonplaceholder.typicode.com/users";
 
 const getPostById = async (id) => {
     try{
@@ -13,4 +13,34 @@ const getPostById = async (id) => {
     }
 }
 
-export { getPostById }
+const getPostsByUsersId = async (userId) => {
+    try{
+        const res = await fetch(`${URL_POSTS}?userId=${userId}`);
+        if (!res.ok) throw new Error(`${res.status}-${res.statusText}`);
+        return res.json();
+    }catch(error){
+        console.error(error.message);
+    }
+}
+
+const getUserById = async (id) => {
+    try{
+        const res = await fetch(`${URL_USERS}/${id}`);
+        if (!res.ok) throw new Error(`${res.status}-${res.statusText}`);
+        return res.json();
+    } catch(error){
+        console.error(error.message);
+    }
+}
+
+const getAllUsers = async () => {
+    try{
+        const res = await fetch(URL_USERS);
+        if (!res.ok) throw new Error(`${res.status}-${res.statusText}`);
+        return res.json();
+    }catch(error){
+        console.error(error.message);
+    }
+}
+
+export { getPostById, getPostsByUsersId, getUserById, getAllUsers }
