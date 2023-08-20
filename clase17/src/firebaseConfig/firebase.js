cdb7605b244da39fc9a0a258525f06e15aaaebe7
Nico Desktop <diaznicox96@gmail.com>
2023-08-20 14:10:10 -0300

    (~) Update

--- clase17/src/firebaseConfig/firebase.js
+++ clase17/src/firebaseConfig/firebase.js
@@ -5,7 +5,14 @@ import { getFirestore } from '@firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {};
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
